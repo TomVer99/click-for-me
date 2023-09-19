b6a878ad079ce926ea71987c454e918310b3d62a
Verlinde,Tom T.S <t.verlinde@student.fontys.nl>
2023-09-19 21:51:55 +0200

    added get_cursor_status() for building detection w/ imgs

---
+++ building_detection/building_detection.py
@@ -0,0 +1,47 @@
+import cv2 as cv
+import numpy as np
+
+__IMAGE_PATH = "building_detection/"
+__THRESHOLD = 0.9
+__MAX_COLOR_DELTA = 10
+__BUILDING_NOT_FOUND = 0
+__BUILDING_NOT_PURCHASABLE = 1
+__BUILDING_PURCHASABLE = 2
+
+def get_cursor_status(screenshot_img:cv.typing.MatLike) -> [int, int, int]:
+    assert screenshot_img is not None, "get_cursor_status() -> screenshot_img is None"
+
+    # load the can_buy image and can_not_buy image
+    can_buy_img = cv.imread(__IMAGE_PATH + "cursor_buy.png", cv.IMREAD_COLOR)
+    can_not_buy_img = cv.imread(__IMAGE_PATH + "cursor_no_buy.png", cv.IMREAD_COLOR)
+    
+    # validate the images
+    assert can_buy_img is not None, "get_cursor_status() -> can_buy_img is None"
+    assert can_not_buy_img is not None, "get_cursor_status() -> can_not_buy_img is None"
+
+    # get the width and height of the images
+    can_buy_img_w, can_buy_img_h = can_buy_img.shape[:-1]
+    can_not_buy_img_w, can_not_buy_img_h = can_not_buy_img.shape[:-1]
+
+    # get the average color of the images
+    can_buy_avg_color = get_avg_color_of_image(can_buy_img)
+    can_not_buy_avg_color = get_avg_color_of_image(can_not_buy_img)
+
+    # find the can_buy image
+    result = cv.matchTemplate(screenshot_img, can_buy_img, cv.TM_CCOEFF_NORMED)
+    locations = np.where(result >= __THRESHOLD)
+    if len(locations[0]) > 0 and len(locations[1]) > 0:
+        location = locations[1][0], locations[0][0]
+        cropped = screenshot_img[location[1]:location[1] + can_buy_img_h, location[0]:location[0] + can_buy_img_w]
+        avg_color = get_avg_color_of_image(cropped)
+        if check_if_color_is_the_same(avg_color, can_buy_avg_color, __MAX_COLOR_DELTA):
+            # We found the can_buy image
+            return [__BUILDING_PURCHASABLE, ( location[0] + ( can_buy_img_w / 2 )), ( location[1] + ( can_buy_img_h / 2 ))]
+        elif check_if_color_is_the_same(avg_color, can_not_buy_avg_color, __MAX_COLOR_DELTA):
+            # We found the can_not_buy image
+            return [__BUILDING_NOT_PURCHASABLE, ( location[0] + ( can_not_buy_img_w / 2 )), ( location[1] + ( can_not_buy_img_h / 2 ))]
+    else:
+        # We are assuming that the image detection does not care for the color,
+        # so if we get here we can assume that the image is not on the screen
+        return [__BUILDING_NOT_FOUND,0,0]
+
