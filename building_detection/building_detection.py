990531398172fd8359761465bf7690b3244f78e1
Verlinde,Tom T.S <t.verlinde@student.fontys.nl>
2023-09-20 13:05:01 +0200

    refactored get_cursor_status() into detect_building()

--- building_detection/building_detection.py
+++ building_detection/building_detection.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 import enum
+import inspect
 
 __IMAGE_PATH = "building_detection/"
 __THRESHOLD = 0.9
@@ -11,42 +12,34 @@ class BuildingStatus(enum.Enum):
     BUILDING_NOT_PURCHASABLE = 1
     BUILDING_PURCHASABLE = 2
 
-# TODO: refactor this function so other buildings are basically copy paste with different images
 def get_cursor_status(screenshot_img:cv.typing.MatLike) -> [int, int, int]:
-    assert screenshot_img is not None, "get_cursor_status() -> screenshot_img is None"
-
-    # load the can_buy image and can_not_buy image
     can_buy_img = cv.imread(__IMAGE_PATH + "cursor_buy.png", cv.IMREAD_COLOR)
     can_not_buy_img = cv.imread(__IMAGE_PATH + "cursor_no_buy.png", cv.IMREAD_COLOR)
+    return detect_building(screenshot_img, can_buy_img, can_not_buy_img)
+
+def detect_building(screenshot_img:cv.typing.MatLike, can_buy_img:cv.typing.MatLike, can_not_buy_img:cv.typing.MatLike
+                    , threshold:float = __THRESHOLD, max_color_delta:int = __MAX_COLOR_DELTA) -> [int, int, int]:
+    assert screenshot_img is not None, f"{inspect.stack()[1][3]}() -> screenshot_img is None"
+    assert can_buy_img is not None, f"{inspect.stack()[1][3]}() -> building_can_buy is None"
+    assert can_not_buy_img is not None, f"{inspect.stack()[1][3]}() -> building_can_not_buy is None"
     
-    # validate the images
-    assert can_buy_img is not None, "get_cursor_status() -> can_buy_img is None"
-    assert can_not_buy_img is not None, "get_cursor_status() -> can_not_buy_img is None"
-
-    # get the width and height of the images
     can_buy_img_w, can_buy_img_h = can_buy_img.shape[:-1]
-
-    # get the average color of the images
     can_buy_avg_color = get_avg_color_of_image(can_buy_img)
     can_not_buy_avg_color = get_avg_color_of_image(can_not_buy_img)
 
-    # find the can_buy image
     result = cv.matchTemplate(screenshot_img, can_buy_img, cv.TM_CCOEFF_NORMED)
-    locations = np.where(result >= __THRESHOLD)
+    locations = np.where(result >= threshold)
     if len(locations[0]) > 0 and len(locations[1]) > 0:
         location = locations[1][0], locations[0][0]
         cropped = screenshot_img[location[1]:location[1] + can_buy_img_h, location[0]:location[0] + can_buy_img_w]
         avg_color = get_avg_color_of_image(cropped)
-        if check_if_color_is_the_same(avg_color, can_buy_avg_color, __MAX_COLOR_DELTA):
-            ret_val = __BUILDING_PURCHASABLE
-        elif check_if_color_is_the_same(avg_color, can_not_buy_avg_color, __MAX_COLOR_DELTA):
-            ret_val = __BUILDING_NOT_PURCHASABLE
-        print(ret_val)
+        if check_if_color_is_the_same(avg_color, can_buy_avg_color, max_color_delta):
+            ret_val = BuildingStatus.BUILDING_PURCHASABLE
+        elif check_if_color_is_the_same(avg_color, can_not_buy_avg_color, max_color_delta):
+            ret_val = BuildingStatus.BUILDING_NOT_PURCHASABLE
         return [ret_val, ( location[0] + ( can_buy_img_w / 2 )), ( location[1] + ( can_buy_img_h / 2 ))]
     else:
-        # We are assuming that the image detection does not care for the color,
-        # so if we get here we can assume that the image is not on the screen
-        return [__BUILDING_NOT_FOUND,0,0]
+        return [BuildingStatus.BUILDING_NOT_FOUND,0,0]
 
 def get_avg_color_of_image(img:cv.typing.MatLike) -> [int, int, int]:
     assert img is not None, "get_avg_color_of_image() -> img is None"
