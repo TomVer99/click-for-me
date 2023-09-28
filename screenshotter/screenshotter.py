9962702416d03b81193d1fa30ba56eddc2ec4eae
Verlinde,Tom T.S <t.verlinde@student.fontys.nl>
2023-09-28 15:50:48 +0200

    no screenshot file gets left behind

--- screenshotter/screenshotter.py
+++ screenshotter/screenshotter.py
@@ -1,9 +1,13 @@
 import pyautogui
 import cv2 as cv
+import os
+
+__SCREENSHOT_FILE_NAME = "automated_screenshot.png"
 
 def take_screenshot() -> cv.typing.MatLike:
     screenshot = pyautogui.screenshot()
-    screenshot.save("automated_screenshot.png")
-    screenshot = cv.imread("automated_screenshot.png", cv.IMREAD_COLOR)
+    screenshot.save(__SCREENSHOT_FILE_NAME)
+    screenshot = cv.imread(__SCREENSHOT_FILE_NAME, cv.IMREAD_COLOR)
+    os.remove(__SCREENSHOT_FILE_NAME)
     assert screenshot is not None, "take_screenshot() -> screenshot is None"
     return screenshot
